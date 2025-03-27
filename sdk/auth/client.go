2230eea7a66eb5bd00b134cec87cbf5f9af94341
Sarikhin <sarikhinlovestory@gmail.com>
2025-03-27 13:13:33 +0700

    [update] add client sdk

--- sdk/auth/client.go
+++ sdk/auth/client.go
@@ -15,7 +15,7 @@ type Client struct {
 }
 
 func NewClient(addr string, timeoutSec int) (*Client, error) {
-	conn, err := grpc.Dial(addr,
+	conn, err := grpc.NewClient(addr,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(10*1024*1024)),
 	)
@@ -44,4 +44,41 @@ func (c *Client) Login(ctx context.Context, email, password string) (string, err
 	}
 
 	return res.Token, nil
+}
+
+func (c *Client) Register(ctx context.Context, req *authv1.RegisterUserRequest) (*authv1.RegisterUserResponse, error) {
+	ctx, cancel := context.WithTimeout(ctx, c.timeout)
+	defer cancel()
+
+	client := authv1.NewAuthServiceClient(c.conn)
+	res, err := client.RegisterUser(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
+
+func (c *Client) ValidateToken(ctx context.Context, token string) (*authv1.ValidateTokenResponse, error) {
+	ctx, cancel := context.WithTimeout(ctx, c.timeout)
+	defer cancel()
+	client := authv1.NewAuthServiceClient(c.conn)
+	res, err := client.ValidateToken(ctx, &authv1.ValidateTokenRequest{
+		Token: token,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (c *Client) AssignRoleToUser(ctx context.Context, req *authv1.AssignRoleRequest) (*authv1.AssignRoleResponse, error) {
+	ctx, cancel := context.WithTimeout(ctx, c.timeout)
+	defer cancel()
+
+	client := authv1.NewAuthServiceClient(c.conn)
+	res, err := client.AssignRoleToUser(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
 }
